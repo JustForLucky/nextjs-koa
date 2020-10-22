0883de9b95dcfa7bf05fafb22bb6f8d77a4bfb4e
lili <751502445@qq.com>
2020-10-22 08:56:09 +0800

    restart

--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,79 @@
-const api = require('../lib/api')
+import { Button } from "antd";
+import getConfig from "next/config";
+import { MailOutlined } from "@ant-design/icons";
+import { connect } from "react-redux";
+const api = require("../lib/api");
 
-const Index = ({ userRepos, userStarredRepos, isLogin }) => {
-    console.log(userRepos, 'userRepos')
-    console.log(userStarredRepos, 'userStarredRepos')
-    console.log(isLogin, 'isLogin')
+const { publicRuntimeConfig } = getConfig();
+
+const Index = ({ userRepos, userStarredRepos, user }) => {
+  if (!user || !user.id) {
     return (
-        <span>Index</span>
-    )
-}
+      <div className="root">
+        <p>亲，您还没有登录哦～</p>
+        <Button type="primary" href={publicRuntimeConfig.OAUTH_URL}>
+          点击登录
+        </Button>
+        <style jsx>{`
+          .root {
+            height: 400px;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+          }
+        `}</style>
+      </div>
+    );
+  }
+  return (
+    <div className="root">
+      <div className="user-info">
+        <img src={user.avatar_url} alt="user avatar" clasName="avatar" />
+        <span className="login">{user.login}</span>
+        <span className="bio">{user.bio}</span>
+        <span className="email">
+          <a href={`mailto:${user.email}`}>
+            <MailOutlined style={{ marginRight: 10 }} />
+            { user.email }
+          </a>
+        </span>
+      </div>
+      <style jsx>{``}</style>
+    </div>
+  );
+};
 
 Index.getInitialProps = async ({ ctx, reduxStore }) => {
-    const user = reduxStore.getState().user
-    if (!user || !user.id) {
-        return {
-            isLogin: false
-        }
-    }
-    const userRepos = await api.request({
-        url: '/user/repos'
-    }, ctx.req, ctx.res)
-    const userStarredRepos = await api.request({
-        url: '/user/starred'
-    }, ctx.req, ctx.res )
+  const user = reduxStore.getState().user;
+  if (!user || !user.id) {
     return {
-        isLogin: true,
-        userRepos: userRepos.data,
-        userStarredRepos: userStarredRepos.data
-    }
-}
+      isLogin: false,
+    };
+  }
+  const userRepos = await api.request(
+    {
+      url: "/user/repos",
+    },
+    ctx.req,
+    ctx.res
+  );
+  const userStarredRepos = await api.request(
+    {
+      url: "/user/starred",
+    },
+    ctx.req,
+    ctx.res
+  );
+  return {
+    isLogin: true,
+    userRepos: userRepos.data,
+    userStarredRepos: userStarredRepos.data,
+  };
+};
 
-export default Index;
+export default connect(function mapState(state) {
+  return {
+    user: state.user,
+  };
+})(Index);
