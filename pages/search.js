5a033bd50408d77c1767b39a6c2ba3ec7ba4ce74
lili <751502445@qq.com>
2020-11-10 08:02:06 +0800

    搜索

--- pages/search.js
+++ pages/search.js
@@ -1,6 +1,8 @@
+import { memo, isValidElement } from 'react'
+import Router, { withRouter } from 'next/router'
 import Link from 'next/link'
-import { withRouter } from 'next/router'
-import { Row, Col, List } from 'antd'
+import { Row, Col, List, Pagination } from 'antd'
+import Repo from '../components/Repo'
 
 const api = require('../lib/api')
 
@@ -20,9 +22,34 @@ const SORT_TYPES = [
  * lang: 仓库项目开发的主语言
  * page: 分页页面
  */
-
+const selectedItemStyle = {
+    borderLeft: '2px solid #e36209',
+    fontWeight: 100
+}
+function noop() {}
+const per_page = 20
+const FilterLink = memo(({ name, nextQuery = {}}) => {
+    const { lang, sort, page, order, query } = nextQuery
+    let queryString = `?query=${query}`
+    if (lang) {
+        queryString += `&lang=${lang}`
+    }
+    if (sort) {
+        queryString += `&sort=${sort}&order=${order || 'desc'}`
+    }
+    if (page) {
+        queryString += `&page=${page}`
+    }
+    queryString += `&per_page=${per_page}`
+    return (
+        <Link href={`/search${queryString}`}>
+            { isValidElement(name) ? name : <a>{name}</a> }
+        </Link>
+    )
+})
 const Search = ({ router, repos }) => {
-    console.log(repos, 'repos')
+    // const { sort, order, lange } = router.query
+    const { page, ...currentQuery } = router.query
     return (
         <div className="root">
             <Row gutter={20}>
@@ -32,19 +59,96 @@ const Search = ({ router, repos }) => {
                         header={<span className="list-header">语言</span>}
                         style={{ marginBottom: 20 }}
                         dataSource={LANGUAGES}
-                        renderItem={lang => (
-                            <List.Item>
-                                <Link href="/search">
-                                    <a>{lang}</a>
-                                </Link>
-                            </List.Item>
-                        )}
+                        renderItem={lang => {
+                            const selected = lang === currentQuery.lang
+                            return (
+                                <List.Item style={ selected ? selectedItemStyle :  {}}>
+                                    {
+                                        selected ? <span>{lang}</span> : (
+                                            <FilterLink
+                                                name={lang}
+                                                nextQuery={{...currentQuery, lang }}
+                                            />
+                                        )
+                                    }
+                                    {/* <a onClick={doSearch.bind(null, { ...currentQuery, lang })}>{lang}</a> */}
+                                </List.Item>
+                            )
+                        }}
                     />
+                    <List
+                        bordered
+                        header={<span className="list-header">排序</span>}
+                        style={{ marginBottom: 20 }}
+                        dataSource={SORT_TYPES}
+                        renderItem={ item => {
+                            let selected = false
+                            if (item.name === 'Best Match' && !currentQuery.sort && !currentQuery.desc) {
+                                selected = true
+                            } else if (currentQuery.sort === item.value && currentQuery.order === item.order) {
+                                selected = true
+                            }
+                            return (
+                                <List.Item style={ selected ? selectedItemStyle : {}}>
+                                    {
+                                        selected ? <span>{item.name}</span> : (
+                                            <FilterLink
+                                                name={item.name}
+                                                nextQuery={{...currentQuery, sort: item.value || '', order: item.order || '' }}
+                                            />
+                                        )
+                                    }
+                                    {/* <a onClick={doSearch.bind(null, { ...currentQuery, sort: item.value || '', order: item.order || '' })}>{item.name}</a> */}
+                                </List.Item>
+                            )
+                        }}
+                    ></List>
                 </Col>
                 <Col span={18}>
-
+                    <h3 className='repos-title'>{repos.total_count}个仓库</h3>
+                    {
+                        repos.items.map(repo => {
+                            return (
+                                <Repo repo={repo} key={repo.id} />
+                            )
+                        })
+                    }
+                    <div className='pagination'>
+                        <Pagination 
+                            pageSize={per_page}
+                            showSizeChanger={false}
+                            current={Number(page) || 1}
+                            total={repos.total_count > 1000 ? 1000 : repos.total_count }
+                            onChange={noop}
+                            itemRender={(page, type, ol) => {
+                                const _page = type === 'page' ? page : type === 'prev' ? page - 1 : page + 1
+                                const name = type === 'page' ? page : ol
+                                return (
+                                    <FilterLink nextQuery={{ ...currentQuery, page }} name={name} />
+                                )
+                            }}
+                        />
+                    </div>
                 </Col>
             </Row>
+            <style jsx>{`
+                .root {
+                    padding: 20px 0;
+                }
+                .list-header {
+                    font-weight: 800;
+                    font-size: 16px;
+                }
+                .repos-title {
+                    border-bottom: 1px solid #eeeeee;
+                    font-size: 24px;
+                    line-height: 50px;
+                }
+                .pagination {
+                    text-align: center;
+                    padding: 20px 0;
+                }
+            `}</style>
         </div>
     )
 }
@@ -70,6 +174,7 @@ Search.getInitialProps = async ({ ctx }) => {
     if (page) {
         queryString += `&page=${page}`
     }
+    queryString += `&per_page=${per_page}`
 
     const result = await api.request({
         url: '/search/repositories' + queryString
